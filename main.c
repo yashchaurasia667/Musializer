6e6f60420dbb011bd8dbc2e960df002c9da16afa
Yash <yashchaurasia630@gmail.com>
2024-10-10 21:08:55 +0530

    figuring out glfw

--- main.c
+++ main.c
@@ -1,36 +1,23 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
-void error_callback(int error, const char *description)
-{
-  fprint(stderr, "Error; %s\n", description);
-}
-
 int main()
 {
-  GLFWwindow *window;
-  glfwSetErrorCallback(error_callback);
-
-  if (!glfwInit())
-  {
-    printf("Failed to initialize GLFW\n");
-    return -1;
-  }
-
-  window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
-  if (!window)
-  {
-    glfwTerminate();
-    return -1;
-  }
+  glfwInit();
+  GLFWwindow *window = glfwCreateWindow(1280, 720, "Todo", NULL, NULL);
+  glfwMakeContextCurrent(window);
 
   while (!glfwWindowShouldClose(window))
   {
     glClear(GL_COLOR_BUFFER_BIT);
-    glfwSwapBuffers(window);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
     glfwPollEvents();
+    glfwSwapBuffers(window);
   }
 
+  glfwDestroyWindow(window);
   glfwTerminate();
+
   return 0;
 }
