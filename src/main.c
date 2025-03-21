b63031f381f71b9b1776333106143704e50e49e4
YASH <yashchaurasia630@gmail.com>
2025-03-21 22:55:42 +0530

    slowing it down

--- src/main.c
+++ src/main.c
@@ -11,11 +11,25 @@
 
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
-uint32_t global_frames[1024] = {0};
+uint32_t global_frames[4080] = {0};
 size_t global_frames_count = 0;
 
 void callback(void *bufferData, unsigned int frames)
 {
+  size_t capacity = ARRAY_LEN(global_frames);
+  if (capacity - global_frames_count >= frames)
+  {
+    memcpy(global_frames + global_frames_count, bufferData, sizeof(int32_t) * frames);
+    global_frames_count += frames;
+  }
+  else if (frames <= capacity)
+  {
+    memmove(global_frames, global_frames + sizeof(uint32_t) * frames, sizeof(uint32_t) * (capacity - frames));
+  }
+  else
+  {
+  }
+
   if (frames > ARRAY_LEN(global_frames))
   {
     frames = ARRAY_LEN(global_frames);
@@ -27,25 +41,28 @@ void callback(void *bufferData, unsigned int frames)
 int main(void)
 {
   /*------------------Initialization-------------------------*/
-
   float volume = 1.0f;
   InitWindow(WINW, WINH, "Musializer");
   SetTargetFPS(FPS);
 
   InitAudioDevice();
-  Music music = LoadMusicStream("src/Valentine.ogg");
+  Music music = LoadMusicStream("src/test.ogg");
 
   assert(music.stream.sampleSize == 16);
   assert(music.stream.channels == 2);
+
+  printf("\n");
   printf("music.frameCount = %u\n", music.frameCount);
   printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
   printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
   printf("music.stream.channels = %u\n", music.stream.channels);
 
+  /*------------------MUSIC CONTROLS-------------------------*/
   PlayMusicStream(music);
   SetMusicVolume(music, volume);
   AttachAudioStreamProcessor(music.stream, callback);
 
+  /*------------------EVENT LOOP-------------------------*/
   while (!WindowShouldClose())
   {
     UpdateMusicStream(music);
@@ -69,8 +86,10 @@ int main(void)
     int w = GetRenderWidth();
     int h = GetRenderHeight();
 
+    /*------------------MUSIC BARS-------------------------*/
     BeginDrawing();
     ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xff});
+
     float cellWidth = (float)w / global_frames_count;
     for (size_t i = 0; i < global_frames_count; i++)
     {
@@ -78,11 +97,13 @@ int main(void)
       if (sample > 0)
       {
         float t = (float)sample / INT16_MAX;
+        // (x, y, width, height, color)
         DrawRectangle(i * cellWidth, h / 2 - h / 2 * t, cellWidth, h / 2 * t, RED);
       }
       else
       {
         float t = (float)sample / INT16_MIN;
+        // (x, y, width, height, color)
         DrawRectangle(i * cellWidth, h / 2, cellWidth, h / 2 * t, RED);
       }
     }
